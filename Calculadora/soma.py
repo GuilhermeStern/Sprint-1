8e30ee98dd31366108634da3449cee3c3390d5ea
Isadora Demoro <isademoro@gmail.com>
2026-08-24 18:03:35 +0000

    Fiz a soma

--- Calculadora/soma.py
+++ Calculadora/soma.py
@@ -1,3 +1,8 @@
 
-def soma():
-    print("Vamos fazer uma soma")
+def soma(num1, num2):
+    return(num1+num2)
+
+#BP
+num1 = float(input("Numero 1: "))
+num2 = float(input("Numero 2: "))
+print(soma(num1,num2))
